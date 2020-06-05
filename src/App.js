aa1e233d9b476562800b479dabc5cad28b2f029e
dima <andreisin2001@mail.ru>
2020-06-05 21:35:55 +0300

    test chart

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Page from './components/layout'
+import chart from "./components/totalProducts";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Page />
+                <div id={chart}/>
+            </div>
+        )
+    }
 }
 
 export default App;
