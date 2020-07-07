e321acefe1689968347f03b736b03c56ef6d445d
dima <andreisin2001@mail.ru>
2020-07-07 13:20:13 +0300

    route protect

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,32 +6,37 @@ import AuthService from "../services/auth.service";
 
 
 const {Header} = Layout;
+
 class Navigbar extends React.Component {
     constructor(props) {
         super(props);
         this.logOut = this.logOut.bind(this);
 
         this.state = {
-            currentUser: false
+            currentUser: undefined
         };
     }
 
-    // componentDidMount() {
-    //     const user = AuthService.getCurrentUser();
-    //
-    //     if (user) {
-    //         this.setState({
-    //             currentUser: user,
-    //         });
-    //     }
-    // }
+    componentDidMount() {
+        const user = AuthService.getCurrentUser();
+
+        if (user) {
+            this.setState({
+                currentUser: user
+            });
+        } else {
+            this.setState({
+                currentUser: undefined
+            })
+        }
+    }
 
     logOut() {
         AuthService.logout();
     }
 
     render() {
-        const { currentUser } = this.state;
+        const {currentUser} = this.state;
 
         return (
             <div>
@@ -47,41 +52,27 @@ class Navigbar extends React.Component {
                             <Menu.Item>
                                 <Link to="/onlineCharts">Online charts</Link>
                             </Menu.Item>
-                            {/*<Menu.Item>*/}
-                            {/*    <Link to="/profile">{currentUser.username}</Link>*/}
-                            {/*</Menu.Item>*/}
-                            {/*<Menu.Item>*/}
-                            {/*    <a href="/" onClick={this.logOut}>*/}
-                            {/*        LogOut*/}
-                            {/*    </a>*/}
-                            {/*</Menu.Item>*/}
-                            {/*<Menu.Item>*/}
-                            {/*    <Link to="/login">Login</Link>*/}
-                            {/*</Menu.Item>*/}
-                            {/*<Menu.Item>*/}
-                            {/*    <Link to="/register">Register</Link>*/}
-                            {/*</Menu.Item>*/}
-                                {currentUser ? (
-                                    <div>
-                                        <Menu.Item>
-                                            <Link to="/profile">{currentUser.username}</Link>
-                                        </Menu.Item>
-                                        <Menu.Item>
-                                            <a href="/" onClick={this.logOut}>
-                                                LogOut
-                                            </a>
-                                        </Menu.Item>
-                                    </div>
-                                ) : (
-                                    <div>
-                                        <Menu.Item>
-                                            <Link to="/login">Login</Link>
-                                        </Menu.Item>
-                                        <Menu.Item>
-                                            <Link to="/register">Register</Link>
-                                        </Menu.Item>
-                                    </div>
-                                )}
+                            {currentUser ? (
+                                <div>
+                                    <Menu.Item>
+                                        <Link to="/profile">{currentUser.username}</Link>
+                                    </Menu.Item>
+                                    <Menu.Item>
+                                        <a href="/" onClick={this.logOut}>
+                                            LogOut
+                                        </a>
+                                    </Menu.Item>
+                                </div>
+                            ) : (
+                                <div>
+                                    <Menu.Item>
+                                        <Link to="/login">Login</Link>
+                                    </Menu.Item>
+                                    <Menu.Item>
+                                        <Link to="/register">Register</Link>
+                                    </Menu.Item>
+                                </div>
+                            )}
                         </Menu>
                     </Header>
                 </Layout>
